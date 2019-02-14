b2360cb2f8bdd846f880372324f4bd305defee60
Padraig McGloin <S00173201@mail.itsligo.ie>
2019-02-14 14:59:46 +0000

    Add project files.

---
+++ Kinect Bluetooth/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+namespace Kinect_Bluetooth
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InfoTextBox = new System.Windows.Forms.TextBox();
+            this.SendTextBox = new System.Windows.Forms.TextBox();
+            this.DevicesListBox = new System.Windows.Forms.ListBox();
+            this.connectionType = new System.Windows.Forms.GroupBox();
+            this.SendRadioButton = new System.Windows.Forms.RadioButton();
+            this.RecieveRadioButton = new System.Windows.Forms.RadioButton();
+            this.GoButton = new System.Windows.Forms.Button();
+            this.connectionType.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // InfoTextBox
+            // 
+            this.InfoTextBox.Location = new System.Drawing.Point(12, 12);
+            this.InfoTextBox.Multiline = true;
+            this.InfoTextBox.Name = "InfoTextBox";
+            this.InfoTextBox.Size = new System.Drawing.Size(637, 291);
+            this.InfoTextBox.TabIndex = 0;
+            // 
+            // SendTextBox
+            // 
+            this.SendTextBox.Location = new System.Drawing.Point(12, 309);
+            this.SendTextBox.Multiline = true;
+            this.SendTextBox.Name = "SendTextBox";
+            this.SendTextBox.Size = new System.Drawing.Size(487, 129);
+            this.SendTextBox.TabIndex = 1;
+            this.SendTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbText_KeyPress);
+            // 
+            // DevicesListBox
+            // 
+            this.DevicesListBox.FormattingEnabled = true;
+            this.DevicesListBox.ItemHeight = 25;
+            this.DevicesListBox.Location = new System.Drawing.Point(505, 309);
+            this.DevicesListBox.Name = "DevicesListBox";
+            this.DevicesListBox.Size = new System.Drawing.Size(283, 129);
+            this.DevicesListBox.TabIndex = 2;
+            this.DevicesListBox.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // connectionType
+            // 
+            this.connectionType.Controls.Add(this.SendRadioButton);
+            this.connectionType.Controls.Add(this.RecieveRadioButton);
+            this.connectionType.Location = new System.Drawing.Point(655, 12);
+            this.connectionType.Name = "connectionType";
+            this.connectionType.Size = new System.Drawing.Size(133, 108);
+            this.connectionType.TabIndex = 3;
+            this.connectionType.TabStop = false;
+            // 
+            // SendRadioButton
+            // 
+            this.SendRadioButton.AutoSize = true;
+            this.SendRadioButton.Location = new System.Drawing.Point(6, 30);
+            this.SendRadioButton.Name = "SendRadioButton";
+            this.SendRadioButton.Size = new System.Drawing.Size(93, 29);
+            this.SendRadioButton.TabIndex = 1;
+            this.SendRadioButton.TabStop = true;
+            this.SendRadioButton.Text = "Send";
+            this.SendRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // RecieveRadioButton
+            // 
+            this.RecieveRadioButton.AutoSize = true;
+            this.RecieveRadioButton.Checked = true;
+            this.RecieveRadioButton.Location = new System.Drawing.Point(6, 65);
+            this.RecieveRadioButton.Name = "RecieveRadioButton";
+            this.RecieveRadioButton.Size = new System.Drawing.Size(121, 29);
+            this.RecieveRadioButton.TabIndex = 0;
+            this.RecieveRadioButton.TabStop = true;
+            this.RecieveRadioButton.Text = "Recieve";
+            this.RecieveRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // GoButton
+            // 
+            this.GoButton.Location = new System.Drawing.Point(680, 126);
+            this.GoButton.Name = "GoButton";
+            this.GoButton.Size = new System.Drawing.Size(75, 33);
+            this.GoButton.TabIndex = 2;
+            this.GoButton.Text = "GO";
+            this.GoButton.UseVisualStyleBackColor = true;
+            this.GoButton.Click += new System.EventHandler(this.bGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GoButton);
+            this.Controls.Add(this.connectionType);
+            this.Controls.Add(this.DevicesListBox);
+            this.Controls.Add(this.SendTextBox);
+            this.Controls.Add(this.InfoTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.connectionType.ResumeLayout(false);
+            this.connectionType.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        
+        #endregion
+
+        private System.Windows.Forms.TextBox InfoTextBox;
+        private System.Windows.Forms.TextBox SendTextBox;
+        private System.Windows.Forms.ListBox DevicesListBox;
+        private System.Windows.Forms.GroupBox connectionType;
+        private System.Windows.Forms.RadioButton SendRadioButton;
+        private System.Windows.Forms.RadioButton RecieveRadioButton;
+        private System.Windows.Forms.Button GoButton;
+    }
+}
+
---
+++ Kinect Bluetooth/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+using System.IO;
+
+namespace Kinect_Bluetooth
+{
+    public partial class Form1 : Form
+    {
+        List<string> items;
+        public Form1()
+        {
+            items = new List<string>();
+            InitializeComponent();
+        }
+
+        private void bGo_Click(object sender, EventArgs e)
+        {
+            if (serverStarted == true)
+            {
+                UpdateUI("Server already started");
+            }
+            if (SendRadioButton.Checked)
+            {
+                StartScan();
+            }
+            else if (RecieveRadioButton.Checked)
+            {
+                ConnectAsServer();
+            }
+            else
+            {
+                UpdateUI("Pick Send or Recieve");
+            }
+        }
+        
+        private void StartScan()
+        {
+            DevicesListBox.DataSource = null;
+            DevicesListBox.Items.Clear();
+            items.Clear();
+            Thread bluetoothScanThread = new Thread(new ThreadStart(Scan));
+            bluetoothScanThread.Start();
+        }
+
+        BluetoothDeviceInfo[] bluetoothDeviceInfos;
+        private void Scan()
+        {
+            UpdateUI("Starting Scan...");
+            BluetoothClient bluetoothClient = new BluetoothClient();
+            bluetoothDeviceInfos = bluetoothClient.DiscoverDevicesInRange();
+            UpdateUI("Scan complete");
+            UpdateUI(bluetoothDeviceInfos.Length.ToString() + " Devices discovered");
+            foreach(BluetoothDeviceInfo d in bluetoothDeviceInfos)
+            {
+                items.Add(d.DeviceName);
+            }
+            UpdateDeviceList();
+        }
+        private void UpdateDeviceList()
+        {
+            Func<int> del = delegate ()
+            {
+                DevicesListBox.DataSource = items;
+                return 0;
+            };
+            Invoke(del);
+        }
+
+        private void ConnectAsServer()
+        {
+            Thread bluetoothServerThread = new Thread(new ThreadStart(ServerConnectThread));
+            bluetoothServerThread.Start();
+        }
+
+        Guid mUUID = new Guid("00001101-0000-1000-8000-00805F9B34FB");
+        bool serverStarted = false;
+        public void ServerConnectThread()
+        {
+            serverStarted = true;
+            UpdateUI("Server started ... Waiting for client");
+            BluetoothListener bluetoothListener = new BluetoothListener(mUUID);
+            bluetoothListener.Start();
+            BluetoothClient bluetoothClient = bluetoothListener.AcceptBluetoothClient();
+            UpdateUI("Server is connected ...");
+            Stream mStream = bluetoothClient.GetStream();
+            while (true)
+            {
+                try
+                {
+                    //handle server connection
+                    byte[] recieved = new byte[1024];
+                    mStream.Read(recieved, 0, recieved.Length);
+                    UpdateUI("Recieved: " + Encoding.ASCII.GetString(recieved) + Environment.NewLine);
+                    byte[] sent = Encoding.ASCII.GetBytes("Data Sent: " + recieved);
+                    mStream.Write(sent, 0, sent.Length);
+                }
+                catch(IOException exception)
+                {
+                    UpdateUI("Client has disconnected");
+                }
+                
+            }
+
+        }
+
+        BluetoothDeviceInfo deviceInfo;
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            deviceInfo = bluetoothDeviceInfos.ElementAt(DevicesListBox.SelectedIndex);
+            UpdateUI(deviceInfo.DeviceName + " was selected, atempting to connect");
+            if(PairDevice())
+            {
+                UpdateUI("device paired ...");
+                UpdateUI("Starting Connect thread");
+                Thread bluetoothClientThread = new Thread(new ThreadStart(ClientConnectThread));
+                bluetoothClientThread.Start();
+            }
+            else
+            {
+                UpdateUI("Pair Failed");
+            }
+        }
+
+        
+        private void ClientConnectThread()
+        {
+            BluetoothClient bluetoothClient = new BluetoothClient();
+            UpdateUI("Atempting connect");
+            bluetoothClient.BeginConnect(deviceInfo.DeviceAddress, mUUID, this.BluetoothClientConnectCallback, bluetoothClient);
+        }
+
+        void BluetoothClientConnectCallback(IAsyncResult asyncResult)
+        {
+            BluetoothClient bluetoothClient = (BluetoothClient)asyncResult.AsyncState;
+            bluetoothClient.EndConnect(asyncResult);
+            Stream stream = bluetoothClient.GetStream();
+            if (bluetoothClient.Connected == true)
+            {
+                UpdateUI("Device Connected");
+            }
+            else
+            {
+                UpdateUI("Device not connected ... try pairing");
+            }
+            if (bluetoothClient.Connected && stream != null)
+            {
+                byte[] buffer = sendData;
+                stream.Write(buffer, 0, buffer.Length);
+                stream.Flush();
+                stream.Close();
+            }
+            if (ready == true)
+            {
+                stream.Write(sendData, 0, sendData.Length);
+            }
+            else
+            {
+                ready = false;
+            }
+            //stream.ReadTimeout = 1000; //read for 1 second
+            /*while (true)
+            {
+                while (!ready) ;
+                stream.Write(sendData, 0, sendData.Length);
+            }*/
+        }
+
+        string myPin = "1234";
+        private bool PairDevice()
+        {
+            if (!deviceInfo.Authenticated)
+            {
+                if(!BluetoothSecurity.PairRequest(deviceInfo.DeviceAddress, myPin))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        bool ready = false;
+        byte[] sendData;
+        private void tbText_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                sendData = Encoding.ASCII.GetBytes("Sent Data: " + SendTextBox.Text); //get text from texbox
+                ready = true;
+                SendTextBox.Clear(); //clear texbox
+            }
+        }
+
+        private void UpdateUI(string message)
+        {
+            Func<int> del = delegate ()
+            {
+                InfoTextBox.AppendText(message + Environment.NewLine);
+                return 0;
+            };
+            Invoke(del);
+        }
+
+    }
+}
